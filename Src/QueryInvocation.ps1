b857cf47a0a466cef0d4f9523e506cd8ff50b5ff
Charlie Billen <charlesbillen@gmail.com>
2020-05-10 17:34:56 +0100

    Adds comment-based help for exposed functions

--- Src/QueryInvocation.ps1
+++ Src/QueryInvocation.ps1
@@ -1,6 +1,79 @@
 . $PSScriptRoot\TokenManagement.ps1
 
 function Invoke-GithubCodeSearch {
+    <#
+    .SYNOPSIS
+    Searches for code on GitHub containing the specified text.
+
+    .DESCRIPTION
+    Searches for code on GitHub containing the specified text.
+
+    Searches can optionally be constrained by language, or to a specific repository
+    or organisation.  These can be combined, for example to search within  a specific language
+    within a specific organisation.
+
+    To search within private repositories or organisations your access token must have been granted
+    access to them.
+
+    By default only the first page of 20 results is returned.  This can be overriden, but GitHub's
+    API will return at most 300 results per page, and at most 1000 results across all pages.
+
+    The function returns a list of results in the format:
+    {
+        Matches: ['', ''],
+        URL: ''
+    }
+    Where Matches is up to 2 text snippets showing the found text in context, and URL is a link
+    to the file on GitHub.
+
+    The results can be passed through to other functions such as Select-Object, or Format-List for
+    further processing.
+
+    .PARAMETER Text
+    The text to search for.
+
+    .PARAMETER Language
+    The language to search in, e.g. C#, JavaScript.
+
+    .PARAMETER Repo
+    The repository to search in.
+
+    .PARAMETER Org
+    The organisation to search in.
+
+    .PARAMETER PerPage
+    The number of results to return, default 20.
+
+    .PARAMETER Page
+    The page of results to return, defaul 1.
+
+    .INPUTS
+    String: The text to search for can be taken from the pipeline.
+
+    .OUTPUTS
+    System.Array: The array of results
+
+    .EXAMPLE
+    Invoke-GithubCodeSearch -Text if
+
+    .EXAMPLE
+    Invoke-GithubCodeSearch -Text if -Language powershell
+
+    .EXAMPLE
+    Invoke-GithubCodeSearch -Text if -Repo charliebillen/GithubCodeSearch
+
+    .EXAMPLE
+    Invoke-GithubCodeSearch -Text if -Org TheDreadPirateRoberts
+
+    .EXAMPLE
+    Invoke-GithubCodeSearch -Text if -PerPage 300
+
+    .EXAMPLE
+    Invoke-GithubCodeSearch -Text if -Page 10
+
+    .LINK
+    https://github.com/charliebillen/GithubCodeSearch
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory, ValueFromPipeline)]
@@ -16,7 +89,6 @@ function Invoke-GithubCodeSearch {
         $Repo,
 
         [Parameter()]
-        [ValidateSet('C#', 'JavaScript', 'Go', 'PowerShell')] # TODO; other languages
         [string]
         $Language,
 
--- Src/TokenManagement.ps1
+++ Src/TokenManagement.ps1
@@ -5,6 +5,28 @@ $GithubCodeSearchTokenConfig = [pscustomobject]@{
 }
 
 function Set-GithubCodeSearchToken {
+    <#
+    .SYNOPSIS
+    Stores the specified GitHub username and password/access token.
+
+    .DESCRIPTION
+    Combines and Base64 encodes the provided username and token ready to use
+    as HTTP Basic Authentication values when calling the GitHub search API.
+
+    The value is stored in the file $HOME\.githubcodesearch_token.
+
+    .PARAMETER Username
+    The GitHub username to use when calling the API.
+
+    .PARAMETER Token
+    The GitHub password/access token to use when calling the API.
+
+    .EXAMPLE
+    Set-GithubCodeSearchToken -Username my_github_username -Token my_github_token
+
+    .LINK
+    https://github.com/charliebillen/GithubCodeSearch
+    #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
         [Parameter(Mandatory)]
@@ -23,6 +45,15 @@ function Set-GithubCodeSearchToken {
     }
 }
 
-function Get-GithubCodeSearchToken {
+function Get-GithubCodeSearchToken {    <#
+    .SYNOPSIS
+    Retrieves the Base64-encoded stored GitHub username and password/access token.
+
+    .DESCRIPTION
+    Reads and returns the contents of $HOME\.githubcodesearch_token
+
+    .LINK
+    https://github.com/charliebillen/GithubCodeSearch
+    #>
     Get-Content -Path $GithubCodeSearchTokenConfig.TokenPath
 }
